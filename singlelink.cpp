cd7688015c9f3a17e892bcf1175c823e0fa42410
panjiprann <panjipranantya20@gmail.com>
2024-04-25 15:49:58 +0700

    case 3-5

--- singlelink.cpp
+++ singlelink.cpp
@@ -171,5 +171,18 @@ int main(){
                 else
                     cout << "Data tidak ditemukan" << endl;
                 break;
-
+              case 3:
+                traverse();
+                break;
+            case 4: 
+                searchData();
+                break;
+            case 5: 
+                break;
+            default:
+                cout << "Pilihan tidak ada" << endl;
+                break;
+            }
+        }
+    
 }
