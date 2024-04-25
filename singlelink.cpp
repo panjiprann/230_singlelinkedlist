6d52004c9c7623092a17f8fcf5d9dd04ac9130b9
panjiprann <panjipranantya20@gmail.com>
2024-04-25 15:45:10 +0700

    membuat case 1

--- singlelink.cpp
+++ singlelink.cpp
@@ -143,6 +143,13 @@ int main(){
             cout << "Pilihan: ";
             cin >> pilihan;
             switch (pilihan)
+            {
             
+              case 1:
+                addNode();
+                cout << "Data Berhasil Ditambahkan" << endl;
+                system("pause");
+                system("cls");
+                break;
 
-        }
+}
